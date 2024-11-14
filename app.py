10d197ef10f6e3a2a441d5b10208be80e3238211
WarrenWu4 <warrenweiwu04@gmail.com>
2024-11-14 09:10:36 -0600

    updated stuff

--- app.py
+++ app.py
@@ -1,75 +1,97 @@
 from flask import Flask, redirect, render_template, request, g, jsonify
 from flask_socketio import SocketIO, emit, join_room, leave_room, rooms
-from models.game import Game
-from utils.colors import print_log, colors
+from models import game, helper
 import uuid
 import sqlite3
-import requests
 import json
-import random
+import base64
+import qrcode
+import io
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 DATABASE = 'database.db'
-CATEGORIES = ['General Knowledge', 'Entertainment', 'Science', 'Mythology', 'Sports', 'Geography', 'History', 'Politics', 'Art', 'Celebrities', 'Animals']
+HELPER = helper.Helper()
 
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
-    cursor = db.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS games (
-                            room_id TEXT PRIMARY KEY,
-                            questions TEXT NOT NULL,
-                            timer INTEGER NOT NULL,
-                            active_players INTEGER NOT NULL,
-                            state TEXT NOT NULL,
-                            current_question INTEGER NOT NULL)''')
-    db.commit()
+    HELPER.create_db(db)
     return db
 
 @app.teardown_appcontext
 def close_connection(exception):
+    if exception:
+        print(exception)
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return redirect('/create')
+    if (request.method == 'POST'):
+        num_questions = request.form.get('questions')
+        room_id = str(uuid.uuid4())
+        game = HELPER.create_room(room_id, num_questions=num_questions)
+        if (game == None):
+            return redirect('/error?msg=Error creating room')
+        game.create_room(get_db())
+        return redirect(f"/game/{room_id}")
+    
+    return render_template('create.html')
 
+@app.route('/game/<room_id>')
+def game(room_id):
+    # check if game already exists
+    game = HELPER.get_room(get_db(), room_id)
+    
+    if (game == None):
+        return redirect('/error?msg=Game does not exist')
+    
+    return render_template('game.html', room_id=room_id, state=game.curr_state)
+
+@app.route('/waiting_room/<room_id>')
+def waiting_room():
+    print(request.path)
+    qr = qrcode.make(request.url_root + request.path)
+    img_io = io.BytesIO()
+    qr.save(img_io, 'PNG')
+    img_io.seek(0)
+    img_base64 = base64.b64encode(img_io.getvalue()).decode()
+    return render_template('waiting_room.html', qrcode=img_base64)
+
+"""
 @app.route('/create', methods=['GET', 'POST'])
 def create():
     if (request.method == 'POST'):
         # get form data
         num_questions = request.form.get('questions')
-        timer = request.form.get('timer')
-        categories = request.form.get('categories')
+        # timer = request.form.get('timer')
+        # categories = request.form.get('categories')
         
         # generate room id
         room_id = str(uuid.uuid4())
         
         # generate questions
-        questions = create_questions(num_questions, categories)
+        data = trivia_data(num_questions).get_trivia_data()
         
         # create game object & store in database
-        game = Game(room_id, questions, timer)
-        game.create_room(get_db())
+        game = Game(room_id, data)
+        res = game.create_room(get_db())
+        if (not res):
+            return redirect('/error?msg=Error creating room')
         
         return redirect(f"/game/{room_id}")
     else:
         return render_template('create.html', categories=CATEGORIES)
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
 
 @app.route('/game/<room_id>')
 def game(room_id):
     game = get_room(room_id)
     if (game is None):
         return redirect('/error?msg=Room not found')
-    return render_template('game.html', game_data=game, shuffle_questions=shuffle_questions)
+    return render_template('game.html', initial_state=game.state)
 
 @app.route('/error')
 def error():
@@ -83,24 +105,49 @@ def check_answer():
     answer = request.form.get('answer')
     game = get_room(room_id)
     if (game is not None):
-        if (game.check_answer(curr_question, answer)):
-            return jsonify({'correct': True})
+        # if (game.check_answer(curr_question, answer)):
+        return jsonify({'correct': True})
     return jsonify({'correct': False})
 
-@socketio.on('add_player')
-def on_add_player(data):
+@app.route('/get_content', methods=['POST'])
+def get_content():
+    state = request.form.get('state')
+    if (state == 'initial'):
+        return render_template('waiting_room.html')
+    else:
+        return render_template('question.html', question="testing")
+    
+@socketio.on('connect_player')
+def on_connect_player(data):
     room_id = data.get('room_id')
-    game = get_room(room_id)
-    game.add_player(get_db())
-    if (game.active_players >= 2):
-        game.start_game(get_db())
+    join_room(room_id)
+    print_log(colors.fg.green, f"Player joined room {room_id}")
+    emit('update_message', {'message': 'shit :)'}, room=room_id)
+    
+@socketio.on('start_game')
+def on_start_game(data):
+    room_id = data.get('room_id')
+    print_log(colors.fg.blue, f"Game ${room_id} starting")
+    emit('start_game', {'html': render_question(room_id, 0)}, room=room_id)
 
-def create_questions(num_questions, categories):
-    url = f'https://opentdb.com/api.php?amount={num_questions}&type=multiple'
-    response = requests.get(url)
-    if (response.status_code == 200):
-        return response.json().get('results')
-    return []
+@socketio.on('new_question')
+def on_new_question(data):
+    room_id = data.get('room_id')
+    question_idx = data.get('question_idx')
+    print_log(colors.fg.cyan, "Generating new question")
+    emit('new_question', {'html': render_question(room_id, question_idx)}, room=room_id)
+
+@socketio.on('leave_game')
+def on_leave_game(data):
+    room_id = data.get('room_id')
+    leave_room(room_id)
+    print_log(colors.fg.red, f"Player left room {room_id}")
+    emit('update_message', {'message': 'goodbye :)'}, room=room_id)
+
+def render_question(room_id, question_idx):
+    game = get_room(room_id)
+    question = base64.b64decode(game.data[question_idx].get('question'))
+    return render_template('question.html', question=question)
 
 def get_room(room_id):
     try:
@@ -108,14 +155,10 @@ def get_room(room_id):
         cursor = db.cursor()
         cursor.execute('SELECT * FROM games WHERE room_id = ?', (room_id,))
         data = cursor.fetchone()
-        return Game(data[0], json.loads(data[1]), data[2], data[3], data[4], data[5])
+        return Game(data[0], json.loads(data[1]), data[2])
     except:
         return None
-    
-def shuffle_questions(correct_ans, incorrect_ans):
-    answers = incorrect_ans + [correct_ans]
-    random.shuffle(answers)
-    return answers
+"""
 
 if __name__ == "__main__":
     app.run(debug=True)
--- models/game.py
+++ models/game.py
@@ -1,48 +1,64 @@
 import json
 
 class Game:
-    def __init__(self, room_id, questions, timer, active_players=0, state='waiting', current_question=1):
+    def __init__(self, room_id, curr_state="waiting", curr_question=0, players=[], questions=[], timer=60):
         self.room_id = room_id
+        self.curr_state = curr_state
+        self.curr_question = curr_question
+        self.players = players
         self.questions = questions
         self.timer = timer
-        self.active_players = active_players
-        self.state = state
-        self.current_question = current_question
-        
+
     def create_room(self, db):
         try:
             cursor = db.cursor()
-            cursor.execute('''INSERT INTO games (room_id, questions, timer, active_players, state, current_question) VALUES (?, ?, ?, ?, ?, ?)''', (self.room_id, json.dumps(self.questions), self.timer, self.active_players, self.state, self.current_question))
-            db.commit()
-            return True
-        except Exception as e:
-            print(e)
-            return False
-    
-    def add_player(self, db):
-        try:
-            self.active_players += 1
-            cursor = db.cursor()
-            cursor.execute('''UPDATE games SET active_players = ? WHERE room_id = ?''', (self.active_players, self.room_id))
-            db.commit()
-            return True
-        except Exception as e:
-            print(e)
-            return False
-    
-    def start_game(self, db):
-        try:
-            self.state = "active"
-            cursor = db.cursor()
-            cursor.execute('''UPDATE games SET state = ? WHERE room_id = ?''', (self.state, self.room_id))
+            cursor.execute(
+                "INSERT INTO games (room_id, curr_state, curr_question, players, questions, timer) VALUES (?, ?, ?, ?, ?, ?)", (self.room_id, self.curr_state, self.curr_question, json.dumps(self.players), json.dumps(self.questions), self.timer)
+            )
             db.commit()
             return True
         except Exception as e:
             print(e)
             return False
         
-    def check_answer(self, curr_question, answer):
-        self.current_question += 1
-        if (self.questions[curr_question-1].get('correct_answer') == answer):
+    def update_state(self, db):
+        try:
+            cursor = db.cursor()
+            cursor.execute(
+                "UPDATE games SET curr_state = ? WHERE room_id = ?", (self.curr_state, self.room_id)
+            )
+            db.commit()
+            return True
+        except Exception as e:
+            print(e)
+            return False
+        
+    def update_player(self, db):
+        try:
+            cursor = db.cursor()
+            cursor.execute(
+                "UPDATE games SET players = ? WHERE room_id = ?", (json.dumps(self.players), self.room_id)
+            )
+            db.commit()
+            return True
+        except Exception as e:
+            print(e)
+            return False
+        
+    def next_question(self, db):
+        try:
+            self.curr_question += 1
+            cursor = db.cursor()
+            cursor.execute(
+                "UPDATE games SET curr_question = ? WHERE room_id = ?", (self.curr_question, self.room_id)
+            )
+            db.commit()
+            return True
+        except Exception as e:
+            print(e)
+            return False
+        
+    def check_answer(self, answer):
+        if (self.questions[self.curr_question]['correct_answer'] == answer):
             return True
         return False
